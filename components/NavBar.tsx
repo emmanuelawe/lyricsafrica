94808adba5e6a09c912b11d0cfa55297c4a6cae9
emmanuelawe <aweemmanuelayobami@gmail.com>
2023-03-01 12:53:35 +0100

    Banner 60%

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,28 +2,30 @@
 
 import Image from 'next/image'
 import Logo from '../public/logo.png'
-import {MenuAlt3Icon, MenuAlt4Icon, SearchIcon} from '@heroicons/react/solid'
+import {MenuIcon, SearchIcon} from '@heroicons/react/solid'
 
 const NavBar = () => {
   return (
     <div>
-    <div className='md:hidden bg-[#1D2025] p-2 border-slate-800 border border-x-0 flex justify-between items-center px-4 md:px-8'>
+    <div className='md:hidden bg-[#1D2025] py-4 space-x-4 flex justify-start items-center'>
         
         {/* Mobile View */}
-
-        <MenuAlt4Icon  className='h-6 w-6 text-white'/>
-       <Image  src={Logo} width={60} height={60} alt='Lyrics Africa Logo' />
+        <div className='flex-1'>
+       <Image  src={Logo} width={60} height={60} alt='Lyrics Africa Logo'  />
+        </div>
         <SearchIcon  className='h-6 w-6 text-white'/>
+        <MenuIcon  className='h-6 w-6 text-white'/>
         </div>
 
         {/* Desktop View */}
-        <div className='hidden bg-[#1D2025] p-2 border-slate-800 border border-x-0 md:flex justify-start space-x-40 items-center px-4 md:px-8'>
+        <div className='hidden py-4 bg-[#1D2025] p-2 md:flex justify-start space-x-40 items-center'>
         
        <Image  src={Logo} width={80} height={80} alt='Lyrics Africa Logo' className='hidden md:flex'/>
         
         <div className='flex-1'>
             <form className='flex space-x-2'>
             <input 
+            type='text'
             className='w-[90%] h-9 bg-transparent focus:outline-none placeholder-slate-500'
             placeholder='Find the lyrics to your favourite songs'/>
             <button type='submit' 
